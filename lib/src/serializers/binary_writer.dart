f61b17eda86c0564e64f0aca3a0ce3410a569c11
ExBrain <zecarlosinf@gmail.com>
2024-07-13 14:00:30 +0100

    Removed unnecessary list

--- lib/src/serializers/binary_writer.dart
+++ lib/src/serializers/binary_writer.dart
@@ -1,13 +1,10 @@
 import 'dart:typed_data';
 
 class BinaryWriter {
-  final List<int> _bytes;
-  final _list = Uint8List(8);
-  late final _view = ByteData.view(_list.buffer);
+  final _bytes = <int>[];
+  final _view = ByteData(8);
 
   Uint8List get bytes => Uint8List.fromList(_bytes);
-
-  BinaryWriter() : _bytes = <int>[];
 
   void writeByte(int byte) {
     _bytes.add(byte);
@@ -19,51 +16,51 @@ class BinaryWriter {
 
   void writeUint8(int value) {
     _view.setUint8(0, value);
-    _bytes.addAll(_list.take(1));
+    _bytes.addAll(_view.buffer.asUint8List().take(1));
   }
 
   void writeUint16(int value) {
     _view.setUint16(0, value, Endian.little);
-    _bytes.addAll(_list.take(2));
+    _bytes.addAll(_view.buffer.asUint8List().take(2));
   }
 
   void writeUint32(int value) {
     _view.setUint32(0, value, Endian.little);
-    _bytes.addAll(_list.take(4));
+    _bytes.addAll(_view.buffer.asUint8List().take(4));
   }
 
   void writeUint64(int value) {
     _view.setUint64(0, value, Endian.little);
-    _bytes.addAll(_list.take(8));
+    _bytes.addAll(_view.buffer.asUint8List().take(8));
   }
 
   void writeInt8(int value) {
     _view.setInt8(0, value);
-    _bytes.addAll(_list.take(1));
+    _bytes.addAll(_view.buffer.asUint8List().take(1));
   }
 
   void writeInt16(int value) {
     _view.setInt16(0, value, Endian.little);
-    _bytes.addAll(_list.take(2));
+    _bytes.addAll(_view.buffer.asUint8List().take(2));
   }
 
   void writeInt32(int value) {
     _view.setInt32(0, value, Endian.little);
-    _bytes.addAll(_list.take(4));
+    _bytes.addAll(_view.buffer.asUint8List().take(4));
   }
 
   void writeInt64(int value) {
     _view.setInt64(0, value, Endian.little);
-    _bytes.addAll(_list.take(8));
+    _bytes.addAll(_view.buffer.asUint8List().take(8));
   }
 
   void writeFloat32(double value) {
     _view.setFloat32(0, value, Endian.little);
-    _bytes.addAll(_list.take(4));
+    _bytes.addAll(_view.buffer.asUint8List().take(4));
   }
 
   void writeFloat64(double value) {
     _view.setFloat64(0, value, Endian.little);
-    _bytes.addAll(_list.take(8));
+    _bytes.addAll(_view.buffer.asUint8List().take(8));
   }
 }
